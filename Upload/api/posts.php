88c7cad7923de83674b9d905f479a254eb5247aa
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2023-01-24 10:00:35 +0100

    Update posts.php
    Added API Authentication & In case a hide tag is used, don't leak the content inside the hide tag anymore.

--- Upload/api/posts.php
+++ Upload/api/posts.php
@@ -18,89 +18,102 @@ if ($mybb->request_method == 'post') {
 
 header('Content-Type: Application/Json');
 
-if ($_GET['id'] || $_GET['tid'] || $_GET['fid']) {
-    $identifier = $_GET['id'] ?? $_GET['tid'];
+$apiKey = $_SERVER['HTTP_API_KEY'] ?? (getallheaders()['Api-Key'] ?? null);
 
-    $isId = false;
-    $isFid = false;
-    if ($_GET['id']) {
-        $isId = true;
-    }
+if ($apiKey) {
+	if ($_GET['id'] || $_GET['tid'] || $_GET['fid']) {
+		$identifier = $_GET['id'] ?? $_GET['tid'];
 
-    if (!$identifier && $_GET['fid']) {
-        $isFid = true;
-        $identifier = $_GET['fid'];
-    }
+		$isId = false;
+		$isFid = false;
+		if ($_GET['id']) {
+			$isId = true;
+		}
 
-    $page = 10 * (($_GET['page'] ?? 0) - 1);
-    $page = $page < 1 ? 0 : $page;
+		if (!$identifier && $_GET['fid']) {
+			$isFid = true;
+			$identifier = $_GET['fid'];
+		}
 
-    $identifier = htmlspecialchars($identifier);
-    $query = $db->simple_select(
-        'posts',
-        '*',
-        $isId
-            ? "pid = $identifier"
-            : ($isFid
-                ? "fid = $identifier"
-                : "tid = $identifier"),
-        [
-            'limit' => 10,
-            'limit_start' => $page,
-            'order_by' => 'dateline',
-            'order_dir' => 'DESC',
-        ]
-    );
+		$page = 10 * (($_GET['page'] ?? 0) - 1);
+		$page = $page < 1 ? 0 : $page;
 
-    if ($isId) {
-        $post = $db->fetch_array($query);
+		$identifier = htmlspecialchars($identifier);
+		$query = $db->simple_select(
+			'posts',
+			'*',
+			$isId
+				? "pid = $identifier"
+				: ($isFid
+					? "fid = $identifier"
+					: "tid = $identifier"),
+			[
+				'limit' => 10,
+				'limit_start' => $page,
+				'order_by' => 'dateline',
+				'order_dir' => 'DESC',
+			]
+		);
 
-        if ($post && $post['visible']) {
-            http_response_code(200);
+		if ($isId) {
+			$post = $db->fetch_array($query);
 
-            $post = new Post($post);
-            echo $post->toJson();
-            return;
-        } else {
-            http_response_code(404);
-            $error = new ApiError(
-                false,
-                "GET request failed: no posts matching given identifier ($identifier)"
-            );
-            $error->display();
-            return;
-        }
-    }
+			if ($post && $post['visible']) {
+				http_response_code(200);
+				$post['message'] = preg_replace("/\[hide\](.*?)\[\/hide\]/", "This content is hidden and can't be shown using the API", $post['message']);
+				$post = new Post($post);
+				echo $post->toJson();
+				return;
+			} else {
+				http_response_code(404);
+				$error = new ApiError(
+					false,
+					"GET request failed: no posts matching given identifier ($identifier)"
+				);
+				$error->display();
+				return;
+			}
+		}
 
-    $num_rows = $db->num_rows($query);
-    if ($num_rows) {
-        $posts = "{ \"posts\": [\n";
+		$num_rows = $db->num_rows($query);
+			if ($num_rows) {
+				$posts = "{ \"posts\": [\n";
 
-        $counter = 0;
-        while ($post = $db->fetch_array($query)) {
-            $post = new Post($post);
-            $posts .= $post->toJson();
+				$counter = 0;
+				while ($post = $db->fetch_array($query)) {
+					$post['message'] = preg_replace("/\[hide\](.*?)\[\/hide\]/", "This content is hidden and can't be shown using the API", $post['message']);
+					$post = new Post($post);
+					$posts .= $post->toJson();
 
-            if ($num_rows != $counter + 1) {
-                $posts .= ",\n";
-            }
+					if ($num_rows != $counter + 1) {
+						$posts .= ",\n";
+					}
 
-            $counter++;
-        }
-        $posts .= "\n], \"count\": $counter }";
-
-        http_response_code(200);
-        echo $posts;
-    } else {
-        http_response_code(404);
-        $error = new ApiError(false, 'GET request failed: no posts were found');
-        $error->display();
-    }
+					$counter++;
+				}
+				$posts .= "\n], \"count\": $counter }";
+				http_response_code(200);
+				echo $posts;
+			
+			} else {
+				http_response_code(404);
+				$error = new ApiError(false, 'GET request failed: no posts were found');
+				$error->display();
+			}
+			
+		} else {
+			http_response_code(400);
+			$error = new ApiError(
+				false,
+				'GET request failed: missing identifier to find a post, please use a thread or forum id'
+			);
+			$error->display();
+		}
+	
 } else {
-    http_response_code(400);
-    $error = new ApiError(
-        false,
-        'GET request failed: missing identifier to find a post, please use a thread or forum id'
-    );
+    http_response_code(401);
+    $error = new ApiError(false, 'Request Failed: API Key is missing');
     $error->display();
 }
+
+
